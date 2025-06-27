559e8c86d5e23a2a70da73caf654eaa15ed8dcaa
Shay Palachy <shaypal5@users.noreply.github.com>
2025-06-27 21:36:56 +0300

    modernize the package

--- src/decore/__init__.py
+++ src/decore/__init__.py
@@ -10,6 +10,4 @@ try:
 except NameError: # pragma: no cover
     pass
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from ._version import *  # noqa: F403
